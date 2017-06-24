d97e3b8bebc83f595bd915f4bd48db378052f2c7
Tony Narlock <tony@git-pull.com>
2017-06-24 03:48:18 -0500

    slim down bootstrap further

--- examples/01_bootstrap.py
+++ examples/01_bootstrap.py
@@ -3,9 +3,7 @@
 from __future__ import (absolute_import, print_function, unicode_literals,
                         with_statement)
 
-import random
-
-from sqlalchemy.orm import joinedload
+from sqlalchemy.sql.expression import func
 
 from unihan_db import bootstrap
 from unihan_db.tables import Unhn
@@ -19,16 +17,8 @@ session = bootstrap.get_session()
 
 bootstrap.bootstrap_unihan(session)
 
-row_count = session.query(Unhn).count()
-
-
-def random_row():
-    return random.randrange(row_count)
-
-
-print(random_row())
-random_row = session.query(Unhn).options(joinedload('kDefinition')).offset(
-    random_row()
+random_row = session.query(Unhn).order_by(
+    func.random()
 ).limit(1).first()
 
 print(random_row.to_dict())
