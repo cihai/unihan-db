b0f764d80bc458e789bb77db489140b24b249de0
Tony Narlock <tony@git-pull.com>
2017-06-24 08:22:13 -0500

    clean up example

--- examples/01_bootstrap.py
+++ examples/01_bootstrap.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf8 - *-
-from __future__ import (absolute_import, print_function, unicode_literals,
-                        with_statement)
+from __future__ import unicode_literals
 
 import pprint
 
@@ -9,11 +8,6 @@ from sqlalchemy.sql.expression import func
 
 from unihan_db import bootstrap
 from unihan_db.tables import Unhn
-
-# If using postgres, example:
-# session = bootstrap.get_session(
-#     'postgresql://postgres:postgres@localhost/unihan_db'
-# )
 
 session = bootstrap.get_session()
 
